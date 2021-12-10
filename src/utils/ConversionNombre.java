b18f7169b11b43c4946c96a4aa769f5b85c25546
Yves LMDG <yves.lmdg@sfr.fr>
2021-12-10 12:04:17 +0100

    TP 4

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		String chaine = "12";
+		int chaineToInt = Integer.parseInt(chaine);
+		int a = 2; 
+		int b = 5;
+		System.out.println("Somme de " + a + " et " + b + " : " + (Integer.sum(a, b)));
+	}
+	
+}
