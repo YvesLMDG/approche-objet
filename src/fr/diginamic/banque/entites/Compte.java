6882577e18008ac47f40ccf925faecf6c52f1639
Yves LMDG <yves.lmdg@sfr.fr>
2021-12-10 12:34:51 +0100

    TP 5

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,13 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	
+	private int numeroCompte;
+	private int soldeCompte;
+	
+	public Compte(int numeroCompte, int soldeCompte) {
+		this.numeroCompte = numeroCompte;
+		this.soldeCompte = soldeCompte;
+	}
+	
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+	
+	public static void main(String[] args) {
+		Compte compte = new Compte(1, 100);
+		
+		System.out.println(compte);
+	}
+	
+}
